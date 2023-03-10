eab1126133b554690273b32e47da1ae570d02ba7
ShravanEssence <shravankumar.essence@gmail.com>
2023-03-10 19:03:03 +0530

    [Added] Logos and made responsive

--- base.js
+++ base.js
@@ -31,3 +31,58 @@ $('.slider').slick({
 		}
 	]
 });
+$('.slider-plan').slick({
+	dots: false,
+	infinite: true,
+	speed: 300,
+	arrows:false,
+	slidesToShow: 3,
+	responsive: [
+	{
+ 		breakpoint: 1024,
+				settings: {
+ 				slidesToShow: 1,
+ 				slidesToScroll: 1,
+ 				infinite: true,
+ 				dots: true
+			}
+	},
+	{
+			breakpoint: 600,
+			settings: {
+ 				slidesToShow: 1,
+ 				slidesToScroll: 1
+			}
+		},
+	{
+		breakpoint: 480,
+			settings: {
+ 				slidesToShow: 1,
+ 				slidesToScroll: 1,
+ 				dots:false
+				}
+		}
+	]
+});
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+$(window).resize(function() {
+    if (window.matchMedia('(max-width: 760px)').matches) {
+        // functionality for screens smaller than 1200px
+        var x = document.getElementById("con");
+        if (x.className === "container") {
+        	x.removeClass("container");
+        	x.addClass("container-fluid");
+        }
+        else {
+        	x.className="container";
+        }
+    }
+});
+
